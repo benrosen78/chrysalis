ac147de4a23547d269f90e6fc293a61bbbb6992d
Adam Demasi <adam@hbang.com.au>
2015-12-12 03:33:59 +1030

    [prefs] use HBSupportController for the support email button

--- prefs/TBCSRootListController.h
+++ prefs/TBCSRootListController.h
@@ -1,6 +1,6 @@
 #import <CepheiPrefs/HBRootListController.h>
 #import <MessageUI/MessageUI.h>
 
-@interface TBCSRootListController : HBRootListController <MFMailComposeViewControllerDelegate>
+@interface TBCSRootListController : HBRootListController
 
 @end
--- prefs/TBCSRootListController.m
+++ prefs/TBCSRootListController.m
@@ -1,4 +1,5 @@
 #include "TBCSRootListController.h"
+#import <CepheiPrefs/HBSupportController.h>
 #import <UIKit/UIImage+Private.h>
 #import <MobileGestalt/MobileGestalt.h>
 
@@ -45,25 +46,8 @@
 }
 
 - (void)showSupportEmailController {
-    MFMailComposeViewController *emailController = [[MFMailComposeViewController alloc] init];
-    emailController.subject = @"Chrysalis Support";
-    emailController.toRecipients = @[@"HASHBANG Productions Support <support@hbang.ws>"];
-
-    NSString *product = (NSString *)MGCopyAnswer(kMGProductType);
-    NSString *version = (NSString *)MGCopyAnswer(kMGProductVersion);
-    NSString *build = (NSString *)MGCopyAnswer(kMGBuildVersion);
-
-    [emailController setMessageBody:[NSString stringWithFormat:@"\n\nCurrent Device: %@, iOS %@ (%@)", product, version, build] isHTML:NO];
-
-    system("/usr/bin/dpkg -l >/tmp/dpkgl.log");
-    [emailController addAttachmentData:[NSData dataWithContentsOfFile:@"/tmp/dpkgl.log"] mimeType:@"text/plain" fileName:@"dpkgl.txt"];
-    [self.navigationController presentViewController:emailController animated:YES completion:nil];
-    emailController.mailComposeDelegate = self;
-    [emailController release];
-}
-
-- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error {
-    [self dismissViewControllerAnimated: YES completion: nil];
+	UIViewController *viewController = (UIViewController *)[HBSupportController supportViewControllerForBundle:[NSBundle bundleForClass:self.class] preferencesIdentifier:@"com.tweakbattles.chrysalis"];
+	[self.navigationController pushViewController:navigationController animated:YES];
 }
 
 @end
